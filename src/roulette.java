0f3725337dfeb0c38fa19a3636591bb99c4c7b8c
PTHy <30455428+PTHy@users.noreply.github.com>
2018-05-07 19:34:39 +0900

    fix rolette error!
    if문을 잘못 적었따!!

--- src/roulette.java
+++ src/roulette.java
@@ -2,25 +2,38 @@
 public class roulette {
 	public static void main(String[] args) {
 		int[] arr = new int [3];
-		int curMon = 300000, seq = 0, ranNum,ranNum2,ranNum3;
-		while(curMon > 0 || curMon < 500000) {
-			curMon -= 1500;
-			
-			ranNum = (int)((Math.random()*7) + 1);
-			ranNum2 = (int)((Math.random()*7) + 1);
-			ranNum3 = (int)((Math.random()*7) + 1);
-			
-			arr[0] = ranNum;
-			arr[1] = ranNum2;
-			arr[2] = ranNum3;
-			if(arr[0] == arr[1]) {
-				if(arr[1] == arr[2]) {
-					System.out.println("arr[0] : "+arr[0]+"arr[1]"+arr[1]+"arr[2]"+arr[2]);
-					curMon += 500000;
-					continue;
+		int curMon = 300000,realseq = 0,seq = 0, ranNum,ranNum2,ranNum3,rich=0,poor=0,win=0,lose=0;
+		while(realseq < 100) {
+			while(curMon > 0 && curMon < 500000) {
+				curMon -= 1500;
+				for(int i = 0; i < 3; i++) {
+					arr[i] = (int)(Math.random()*8+1);	
 				}
+				
+				if(arr[0] == arr[1]) {
+					if(arr[1] == arr[2]) {
+						curMon += 100000;
+						win++;
+						continue;
+					}
+				}
+				seq++;
+				lose++;
 			}
-			seq++;
+			if(curMon > 500000) {
+				System.out.println(realseq + "번 : 부자가 됬어요!");
+				rich++;
+			}
+			else {
+				System.out.println(realseq + "번 : 다 잃었어요...");
+				poor++;
+			}
+			realseq++;
+			seq = 0;
+			curMon = 300000;
 		}
+		System.out.println("\n===============================\n");
+		System.out.printf("%d번 이겼고 %d번 졌습니다. 승률은 %.2f%% 입니다.\n",win,lose,(((double)win/(win+lose))*100));
+		System.out.println(rich+"번 부자가 되었고 "+poor+"번 돈을 모두 잃었습니다. 부자가 된 확률은 "+((double)rich/realseq)*100+"% 입니다.");
 	}
 }
